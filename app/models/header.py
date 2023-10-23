34cd5a9cc8789c89ff98c8cbfc76836201b5e981
MoguchiyDD <ket-ket-01@bk.ru>
2023-10-23 20:34:39 +0600

    Adding the «app/models/header.py» file + template

---
+++ app/models/header.py
@@ -0,0 +1,84 @@
+# Developer && Owner: МогучийДД (MoguchiyDD)
+# LICENSE: MIT License which is located in the text file LICENSE
+#
+# Goal: Create a HEADER TEMPLATE with Ready-Made Working Filling
+# Result: Providing a HEADER TEMPLATE
+#
+# Past Modification: Adding COPYRIGHT
+# Last Modification: Adding The «Header» CLASS
+# Modification Date: 2023.10.23, 08:31 PM
+#
+# Create Date: 2023.10.23, 06:45 PM
+
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import (
+    QWidget,
+	QFrame,
+	QHBoxLayout,
+	QLabel,
+	QPushButton
+)
+
+from .values import string_values
+
+
+# ------------ HEADER ------------
+
+class Header(QWidget):
+	"""
+	--
+    PARAMETER:
+    - parent: QWidget | None = None -> Widget PARENT for this CLASS
+    (AFTER THAT, THIS CURRENT CLASS WILL BECOME A CHILD OF THE PARENT)
+    - f: Qt.WindowType = Qt.WindowType.Widget -> Window-System (Widget)
+	---
+	FUNCTIONS:
+	- page() -> QFrame : Create 1 HEADER FRAME
+	"""
+
+	def __init__(
+        self,
+        parent: QWidget | None = None,
+        flags: Qt.WindowType = Qt.WindowType.Widget
+    ) -> None:
+		super(Header, self).__init__(parent, flags)
+		self.setParent(parent)
+
+		template = self.page()
+		parent.main_layout.addWidget(
+			template, alignment=Qt.AlignmentFlag.AlignTop
+		)
+
+	def page(self) -> QFrame:
+		"""
+		Create 1 HEADER TEMPLATE
+
+		---
+		RESULT: HEADER TEMPLATE
+		"""
+
+		frame = QFrame()
+		frame.setObjectName("header")
+
+		layout = QHBoxLayout()
+
+		text = string_values("header_title")
+		title = QLabel(text)
+
+		btn_settings = QPushButton()
+		btn_settings.setObjectName("header_btn_settings")
+		btn_settings.setFixedWidth(25)
+
+		btn_license = QPushButton()
+		btn_license.setObjectName("header_btn_license")
+		btn_license.setFixedWidth(25)
+
+		layout.addWidget(title, alignment=Qt.AlignmentFlag.AlignLeft)
+		layout.addWidget(btn_settings, alignment=Qt.AlignmentFlag.AlignRight)
+		layout.addWidget(btn_license)
+
+		frame.setLayout(layout)
+		return frame
+
+# --------------------------------
