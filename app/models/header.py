267261a68178fd02baa9888e72a0b28f11c0134d
MoguchiyDD <ket-ket-01@bk.ru>
2023-10-23 21:00:18 +0600

    Checking CODE The PEP8

--- app/models/header.py
+++ app/models/header.py
@@ -4,9 +4,9 @@
 # Goal: Create a HEADER TEMPLATE with Ready-Made Working Filling
 # Result: Providing a HEADER TEMPLATE
 #
-# Past Modification: Adding COPYRIGHT
-# Last Modification: Adding The «Header» CLASS
-# Modification Date: 2023.10.23, 08:31 PM
+# Past Modification: Adding The «Header» CLASS
+# Last Modification: Checking CODE The PEP8
+# Modification Date: 2023.10.23, 09:00 PM
 #
 # Create Date: 2023.10.23, 06:45 PM
 
@@ -14,10 +14,10 @@
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (
     QWidget,
-	QFrame,
-	QHBoxLayout,
-	QLabel,
-	QPushButton
+    QFrame,
+    QHBoxLayout,
+    QLabel,
+    QPushButton
 )
 
 from .values import string_values
@@ -26,59 +26,61 @@ from .values import string_values
 # ------------ HEADER ------------
 
 class Header(QWidget):
-	"""
-	--
+    """
+    Providing a HEADER TEMPLATE
+
+    ---
     PARAMETER:
     - parent: QWidget | None = None -> Widget PARENT for this CLASS
     (AFTER THAT, THIS CURRENT CLASS WILL BECOME A CHILD OF THE PARENT)
     - f: Qt.WindowType = Qt.WindowType.Widget -> Window-System (Widget)
-	---
-	FUNCTIONS:
-	- page() -> QFrame : Create 1 HEADER FRAME
-	"""
+    ---
+    FUNCTIONS:
+    - page() -> QFrame : Create 1 HEADER FRAME
+    """
 
-	def __init__(
+    def __init__(
         self,
         parent: QWidget | None = None,
         flags: Qt.WindowType = Qt.WindowType.Widget
     ) -> None:
-		super(Header, self).__init__(parent, flags)
-		self.setParent(parent)
+        super(Header, self).__init__(parent, flags)
+        self.setParent(parent)
 
-		template = self.page()
-		parent.main_layout.addWidget(
-			template, alignment=Qt.AlignmentFlag.AlignTop
-		)
+        template = self.page()
+        parent.main_layout.addWidget(
+            template, alignment=Qt.AlignmentFlag.AlignTop
+        )
 
-	def page(self) -> QFrame:
-		"""
-		Create 1 HEADER TEMPLATE
+    def page(self) -> QFrame:
+        """
+        Create 1 HEADER TEMPLATE
 
-		---
-		RESULT: HEADER TEMPLATE
-		"""
+        ---
+        RESULT: HEADER TEMPLATE
+        """
 
-		frame = QFrame()
-		frame.setObjectName("header")
+        frame = QFrame()
+        frame.setObjectName("header")
 
-		layout = QHBoxLayout()
+        layout = QHBoxLayout()
 
-		text = string_values("header_title")
-		title = QLabel(text)
+        text = string_values("header_title")
+        title = QLabel(text)
 
-		btn_settings = QPushButton()
-		btn_settings.setObjectName("header_btn_settings")
-		btn_settings.setFixedWidth(25)
+        btn_settings = QPushButton()
+        btn_settings.setObjectName("header_btn_settings")
+        btn_settings.setFixedWidth(25)
 
-		btn_license = QPushButton()
-		btn_license.setObjectName("header_btn_license")
-		btn_license.setFixedWidth(25)
+        btn_license = QPushButton()
+        btn_license.setObjectName("header_btn_license")
+        btn_license.setFixedWidth(25)
 
-		layout.addWidget(title, alignment=Qt.AlignmentFlag.AlignLeft)
-		layout.addWidget(btn_settings, alignment=Qt.AlignmentFlag.AlignRight)
-		layout.addWidget(btn_license)
+        layout.addWidget(title, alignment=Qt.AlignmentFlag.AlignLeft)
+        layout.addWidget(btn_settings, alignment=Qt.AlignmentFlag.AlignRight)
+        layout.addWidget(btn_license)
 
-		frame.setLayout(layout)
-		return frame
+        frame.setLayout(layout)
+        return frame
 
 # --------------------------------
