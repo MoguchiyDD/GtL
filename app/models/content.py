9eaa6eb5cb9c113c0a5c65776caf95effa792f69
MoguchiyDD <ket-ket-01@bk.ru>
2023-10-24 17:42:30 +0600

    Adding 1 CONTENT TEMPLATE

---
+++ app/models/content.py
@@ -0,0 +1,65 @@
+# Developer && Owner: МогучийДД (MoguchiyDD)
+# LICENSE: MIT License which is located in the text file LICENSE
+#
+# Goal: Create a CONTENT TEMPLATE with Ready-Made Working Filling
+# Result: Providing a CONTENT TEMPLATE
+#
+# Past Modification: Adding COPYRIGHT
+# Last Modification: Adding The «Content» CLASS
+# Modification Date: 2023.10.24, 05:41 PM
+#
+# Create Date: 2023.10.24, 05:39 PM
+
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QWidget, QFrame, QHBoxLayout
+
+from .values import string_values
+
+
+# ------------ CONTENT ------------
+
+class Content(QWidget):
+    """
+    Providing a CONTENT TEMPLATE
+
+    ---
+    PARAMETER:
+    - parent: QWidget | None = None -> Widget PARENT for this CLASS
+    (AFTER THAT, THIS CURRENT CLASS WILL BECOME A CHILD OF THE PARENT)
+    - f: Qt.WindowType = Qt.WindowType.Widget -> Window-System (Widget)
+    ---
+    FUNCTIONS:
+    - page() -> QFrame : Create 1 CONTENT FRAME
+    """
+
+    def __init__(
+        self,
+        parent: QWidget | None = None,
+        flags: Qt.WindowType = Qt.WindowType.Widget
+    ) -> None:
+        super(Content, self).__init__(parent, flags)
+        self.setParent(parent)
+
+        template = self.page()
+        parent.main_layout.addWidget(
+            template, alignment=Qt.AlignmentFlag.AlignBottom
+        )
+
+    def page(self) -> QFrame:
+        """
+        Create 1 CONTENT TEMPLATE
+
+        ---
+        RESULT: CONTENT TEMPLATE
+        """
+
+        frame = QFrame()
+        frame.setObjectName("content")
+
+        layout = QHBoxLayout()
+
+        frame.setLayout(layout)
+        return frame
+
+# ---------------------------------
