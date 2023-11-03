15d8d2413149840fb51f36b378d848294b2625d7
MoguchiyDD <ket-ket-01@bk.ru>
2023-11-04 00:46:39 +0600

    Adding FYLE SYSTEM with 1st TEMPLATE

---
+++ app/models/filesystem.py
@@ -0,0 +1,90 @@
+# Developer && Owner: МогучийДД (MoguchiyDD)
+# LICENSE: MIT License which is located in the text file LICENSE
+#
+# Goal: Writing and Reading IMPORTANT FILES
+# Result: AUTOMATED SYSTEM with FILES
+#
+# Past Modification: Adding The «FileSystem» CLASS
+# Last Modification: Checking CODE The PEP8
+# Modification Date: 2023.11.04, 12:43 AM
+#
+# Create Date: 2023.11.01, 10:01 PM
+
+
+from json import dumps, loads
+from os import path, chdir, mkdir
+
+
+# ------------ FILE SYSTEM ------------
+
+class FyleSystem:
+    """
+    Storage of SYSTEM FILES
+
+    ---
+    PARAMETERS:
+    - path_main_file: str -> The «basedir» VARIABLE from The «app/main.py» FILE
+    ---
+    FUNCTIONS:
+    - write_file_settings(self, data: str) -> None : Writes The SETTINGS FILE
+    - read_file_settings(self) -> dict[str, any] : Reads The SETTINGS FILE
+    """
+
+    FOLDER = ".settings"
+    FILE_SETTINGS = ".settings.json"
+    TEMPLATE = {
+        "dash": True,
+        "block": ". ? !"
+    }
+
+    def __init__(self, path_main_file: str) -> None:
+        self.basedir = path_main_file
+
+        self.path_file_system = path.dirname(__file__)
+        self.path_folder_settings = path.join(path_main_file, self.FOLDER)
+        self.path_file_settings = path.join(
+            self.path_folder_settings, self.FILE_SETTINGS
+        )
+
+        self.__check_existence_folder()
+
+    def __check_existence_folder(self) -> None:
+        """
+        Creates 1 FOLDER for The SETTINGS FILE if The FOLDER does not Exist and
+        Writes The Finished TEMPLATE inside The FILE
+        """
+
+        if path.exists(self.path_folder_settings) is False:
+            chdir(self.basedir)
+            mkdir(self.FOLDER)
+            chdir(self.path_file_system)
+            self.write_file_settings(dumps(self.TEMPLATE))
+        else:
+            chdir(self.path_file_system)
+
+    def write_file_settings(self, data: str) -> None:
+        """
+        Writes The SETTINGS FILE
+
+        ---
+        PARAMETERS:
+        - data: str -> The DATA for SETTINGS FILE
+        """
+
+        with open(self.path_file_settings, "w+") as f:
+            f.write(data)
+
+    def read_file_settings(self) -> dict[str, any]:
+        """
+        Reads The SETTINGS FILE
+
+        ---
+        RESULT: The DATA from SETTINGS FILE
+        """
+
+        with open(self.path_file_settings, "r") as f:
+            data = f.read()
+
+        return loads(data)
+
+# -------------------------------------
