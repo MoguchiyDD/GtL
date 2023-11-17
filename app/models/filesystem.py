3c17db4d794566acdcfe6f2b8ce5e3a24a614272
MoguchiyDD <ket-ket-01@bk.ru>
2023-11-17 23:55:34 +0600

    Checking VALUES

--- app/models/filesystem.py
+++ app/models/filesystem.py
@@ -4,9 +4,9 @@
 # Goal: Writing and Reading IMPORTANT FILES
 # Result: AUTOMATED SYSTEM with FILES
 #
-# Past Modification: Editing The «FileSystem» CLASS («write_file_settings»)
-# Last Modification: Editing The «FileSystem» CLASS («TEMPLATE»)
-# Modification Date: 2023.11.14, 01:02 AM
+# Past Modification: Editing The «FileSystem» CLASS («_valid_true_values»)
+# Last Modification: Checking CODE The PEP8
+# Modification Date: 2023.11.17, 11:47 PM
 #
 # Create Date: 2023.11.01, 10:01 PM
 
@@ -29,8 +29,10 @@ class FileSystem:
     a PROBLEM with The KEYS
     ---
     FUNCTIONS:
-    - _valid_true_keys() -> bool : Checks The TITLE of The KEYS for Correct
-    Spelling
+    - _valid_true_keys(data: list[str]) -> bool : Checks The TITLE of The KEYS
+    for Correct Spelling
+    - _valid_true_values(data: dict[str, any]) -> bool : Checks The TITLE of
+    The VALUES for Correct Spelling
     - _check_existence_folder(self) -> None : Creates 1 FOLDER for The SETTINGS
     FILE if The FOLDER does not Exist and Writes The Finished TEMPLATE inside
     The FILE
@@ -41,14 +43,31 @@ class FileSystem:
     - read_file_settings(self) -> dict[str, any] : Reads The SETTINGS FILE
     """
 
+    # MAIN
     FOLDER = ".settings"
     FILE_SETTINGS = ".settings.json"
+
+    # KEYS
+    TEMPLATE_TITLE_KEY = "title"
+    TEMPLATE_LIST_KEY = "list"
+    TEMPLATE_DASH_KEY = "dash"
+    TEMPLATE_BLOCK_KEY = "block"
+    TEMPLATE_LANGUAGE_KEY = "language"
+
+    # VALUES
+    TEMPLATE_TITLE_VALUE = False
+    TEMPLATE_LIST_VALUE = False
+    TEMPLATE_DASH_VALUE = True
+    TEMPLATE_BLOCK_VALUE = [".", "?", "!"]
+    TEMPLATE_LANGUAGE_VALUE = "RU"
+
+    # TEMPLATE
     TEMPLATE = {
-        "title": False,
-        "list": False,
-        "dash": True,
-        "block": [".", "?", "!"],
-        "language": "RU"
+        TEMPLATE_TITLE_KEY: TEMPLATE_TITLE_VALUE,
+        TEMPLATE_LIST_KEY: TEMPLATE_LIST_VALUE,
+        TEMPLATE_DASH_KEY: TEMPLATE_DASH_VALUE,
+        TEMPLATE_BLOCK_KEY: TEMPLATE_BLOCK_VALUE,
+        TEMPLATE_LANGUAGE_KEY: TEMPLATE_LANGUAGE_VALUE
     }
     TEMPLATE_KEYS = TEMPLATE.keys()
 
@@ -95,6 +114,92 @@ class FileSystem:
             except:
                 result = False
                 return result
+
+        return result
+
+    def _valid_true_values(self, data: dict[str, any]) -> bool:
+        """
+        Checks The TITLE of The VALUES for Correct Spelling
+
+        ---
+        PARAMETERS:
+        - data: dict[str, any] -> The DATA with CURRENT DICTIONARY from RAM
+        ---
+        RESULT: True (RIGHT VALUES) || False (NOT RIGHT VALUES)
+        """
+
+        def __bool(value: bool, _cnt_result: int) -> int:
+            """
+            Checking a BOOLEAN VALUE
+
+            ---
+            PARAMETERS:
+            - value: bool -> The VALUE from The KEY
+            - _cnt_result: int -> Number of ERRORS to be LOGGED if ERRORS Exist
+            ---
+            RESULT: Number of ERRORS when Checking a VALUE by KEY
+            """
+
+            if isinstance(value, bool) is False:
+                _cnt_result += 1
+
+            return _cnt_result
+
+        def __block(value: bool, _cnt_result: int) -> int:
+            """
+            Checking The VALUE of The «block» KEY
+
+            ---
+            PARAMETERS:
+            - value: bool -> The VALUE from The KEY
+            - _cnt_result: int -> Number of ERRORS to be LOGGED if ERRORS Exist
+            ---
+            RESULT: Number of ERRORS when Checking a VALUE by KEY
+            """
+
+            if isinstance(value, list) is False:
+                _cnt_result += 1
+            else:
+                for val in value:
+                    if isinstance(val, str) is False:
+                        _cnt_result += 1
+
+            return _cnt_result
+
+        def __language(value: bool, _cnt_result: int) -> int:
+            """
+            Checking The VALUE of The «language» KEY
+
+            ---
+            PARAMETERS:
+            - value: bool -> The VALUE from The KEY
+            - _cnt_result: int -> Number of ERRORS to be LOGGED if ERRORS Exist
+            ---
+            RESULT: Number of ERRORS when Checking a VALUE by KEY
+            """
+
+            if value not in ["RU", "EN"]:
+                _cnt_result += 1
+
+            return _cnt_result
+
+        result = True
+        _cnt_result = 0
+
+        for key, value in data.items():
+            if key == self.TEMPLATE_TITLE_KEY:  # title
+                _cnt_result = __bool(value, _cnt_result)
+            elif key == self.TEMPLATE_LIST_KEY:  # list
+                _cnt_result = __bool(value, _cnt_result)
+            elif key == self.TEMPLATE_DASH_KEY:  # dash
+                _cnt_result = __bool(value, _cnt_result)
+            elif key == self.TEMPLATE_BLOCK_KEY:  # block
+                _cnt_result = __block(value, _cnt_result)
+            elif key == self.TEMPLATE_LANGUAGE_KEY:  # language
+                _cnt_result = __language(value, _cnt_result)
+
+        if _cnt_result >= 1:
+            result = False
 
         return result
 
